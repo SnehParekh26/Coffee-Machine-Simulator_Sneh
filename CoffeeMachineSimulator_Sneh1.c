c3cd45a0310b755967a6172f29715955c1db475e
SnehParekh26 <146330905+SnehParekh26@users.noreply.github.com>
2024-09-27 13:48:39 +0530

    Add files via upload

---
+++ CoffeeMachineSimulator_Sneh1.c
@@ -0,0 +1,14 @@
+# Sneh_CSCI291_2024
+#include <stdio.h>   // Standard input-output library for I/O functions
+
+float total_amount = 0;
+int coffee_beans = 100;            
+int water = 500;                  
+int milk = 500;                   
+int chocolate = 200;               
+
+// Function declarations
+void orderCoffee();    
+void adminMode();      
+void displayMenu();    
+void checkLowIngredients();  
